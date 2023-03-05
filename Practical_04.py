4b5828370a857ab7539d9cf3c080188b8e162671
Aryan Chandra <aryanchandra7360@gmail.com>
2023-03-05 19:58:27 +0530

    One More Down

--- Practical_04.py
+++ Practical_04.py
@@ -1,2 +1,22 @@
 # Create/Define single dimension / multi-dimension arrays,
-# and arrays with specific values like array of all ones, all zeros, array with random values within a range, or a diagonal matrix
+# and arrays with specific values like array of all ones, all zeros, array with random values within a range, or a diagonal matrix.
+
+import numpy as np
+
+singleArray = np.array([])
+
+multiArray = np.array([], [])
+
+allOneArray = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+
+allZeroArray = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+randArray = np.array([])
+
+for x in range(0, 1000):
+    temp = np.random.randint(100000)
+    print(temp)
+    randArray = np.append(randArray, temp)
+
+value = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+diagMatrix = np.diag(value)
