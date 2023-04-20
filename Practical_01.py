6bd598d83a8ac5314e28b29b1af7052575807cf4
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-04-20 10:17:38 +0530

    Update Practical_01.py
    I we need to implement elementary mathematical operations, then I believe for exponentation we need to implement b^a.

--- Practical_01.py
+++ Practical_01.py
@@ -1,8 +1,6 @@
 # Perform elementary mathematical operations in Octave/MATLAB like addition, 
 # multiplication, division and exponentiation.
 
-import math
-
 a = 100 
 b = 200
 
@@ -10,4 +8,4 @@ print("Sum : " , b+a)
 print("Subtraction : " , b-a)
 print("Multiplication : " , b*a)
 print("Division : " , b/a)
-print("Exponent : " , math.exp(100))
+print("Exponent : " , b**a)
