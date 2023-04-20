5c98566314bc293445b554021133a663b3857691
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-04-20 11:45:43 +0530

    Update Practical_10.py
    This match the requirements.

--- Practical_10.py
+++ Practical_10.py
@@ -1,1 +1,35 @@
 # Use conditional statements and different type of loops based on simple example/
+
+def check_grade(score):
+    if score >= 90:
+        grade = "A"
+    elif score >= 80:
+        grade = "B"
+    elif score >= 70:
+        grade = "C"
+    elif score >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade
+
+def print_table(number):
+    for i in range(1, 11):
+        print(number, "x", i, "=", number * i)
+
+def calculate_factorial(number):
+    factorial = 1
+    while number > 0:
+        factorial = factorial * number
+        number = number - 1
+    return factorial
+
+score = int(input("Enter your score: "))
+grade = check_grade(score)
+print("Your grade is", grade)
+
+number = int(input("Enter a number: "))
+print_table(number)
+
+factorial = calculate_factorial(number)
+print("The factorial is", factorial)
