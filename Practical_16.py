f0cff564f626d93d8da720409eb87bcc0d884d01
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-04-20 13:15:02 +0530

    Update Practical_16.py
    This is more cleaner

--- Practical_16.py
+++ Practical_16.py
@@ -6,10 +6,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt 
 
 df = pd.read_csv('student_data.csv')
 
 df['gender'] = df['gender'].map({'F':0, 'M':1})
+
 X = df[['gender', 'height', 'weight', 'math_score', 'reading_score', 'writing_score']] 
 y = df['activity'] 
 
@@ -23,15 +25,21 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=100, batch_size=10)
+history = model.fit(X_train, y_train, epochs=100, batch_size=10, verbose=0) 
+
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
 
 _, acc = model.evaluate(X_test, y_test)
-print('Accuracy: %.2f' % (acc*100))
 
 y_pred = model.predict(X_test)
 y_pred = np.round(y_pred)
 
 cm = confusion_matrix(y_test, y_pred)
 cr = classification_report(y_test, y_pred)
-print('Confusion matrix:\n', cm)
-print('Classification report:\n', cr)
+print('Confusion matrix:\n', cm) 
+print('Classification report:\n', cr) 
